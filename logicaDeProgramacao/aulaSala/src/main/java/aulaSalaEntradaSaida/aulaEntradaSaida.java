97765d58c169f7451cc7cbfb34bab6c33f1db669
EstherPimentel <85425371+EstherPimentel@users.noreply.github.com>
2022-04-22 16:10:56 -0300

    commit mudanças

---
+++ logicaDeProgramacao/aulaSala/src/main/java/aulaSalaEntradaSaida/aulaEntradaSaida.java
@@ -0,0 +1,20 @@
+package aulaSalaEntradaSaida;
+
+import java.util.Scanner;
+
+public class aulaEntradaSaida {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Imprimindo com printLN");
+        System.out.print("Imprimindo na mesma linha\n");
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.printf("O %s tem %d anos", nome, idade);
+        System.out.println("A " + nome + " tem " + idade + " anos.");
+    }
+}
