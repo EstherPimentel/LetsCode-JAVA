96218af723764ce953fed64e03b5607e3d0252cd
EstherPimentel <85425371+EstherPimentel@users.noreply.github.com>
2022-04-25 00:27:12 -0300

    exrcícios feitos

--- logicaDeProgramacao/aula04/src/main/java/aulaSalaMatriz/aulaMatriz.java
+++
@@ -1,7 +0,0 @@
-package aulaSalaMatriz;
-
-public class aulaMatriz {
-    public static void main(String[] args) {
-        int temperaturasPorEstacao[][] = {{38, 28, 27, 30}, {35, 22, 21, 19}};
-    }
-}
